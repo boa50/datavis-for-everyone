d5fd2228d58c9474f9b663b5940af53ef6880d2f
boa50 <bruno.albuquerque50@gmail.com>
2024-04-10 10:43:05 -0300

    Added a temperature area to the line chart

--- napoleon-russia-march-rebuild/charts/line/script.js
+++ napoleon-russia-march-rebuild/charts/line/script.js
@@ -4,15 +4,22 @@ import { colours } from '../../constants.js'
 const getData = () =>
     Promise.all([
         d3.csv('./data/troops.csv')
-            .then(d => d.filter(v => (v.direction === 'Retreating') & (v.group == 1))),
+            .then(d => d
+                .filter(v => v.direction === 'Retreating')
+                .map(v => { return { ...v, long: +v.long, survivors: +v.survivors } })),
         d3.csv('./data/temperatures.csv')
+            .then(d =>
+                [{ ...d[0], long: 37.8885 }].concat(
+                    d.map(v => { return { ...v, long: +v.long, 'temp C': +v['temp C'] } })
+                ).concat({ ...d[d.length - 1], long: 23.9795 })
+            )
     ])
 
 const svgWidth = 1080
 const svgHeight = 720
 const marginDefault = {
     left: 64,
-    right: 16,
+    right: 64,
     top: 16,
     bottom: 64
 }
@@ -38,6 +45,7 @@ const getSvgChart = (id, marginCustom = {}) => {
 getData().then(datasets => {
     const data = datasets[0]
     const temperatures = datasets[1]
+    const dataPerGroup = d3.group(data, d => d.group)
 
     const [chart, width, height] = getSvgChart('line-chart')
 
@@ -46,22 +54,78 @@ getData().then(datasets => {
         .domain(d3.extent(data, d => d.long).map((d, i) => d * [0.995, 1.005][i]).reverse())
         .range([0, width])
 
-    const y = d3
+    const ySurvivors = d3
         .scaleLinear()
         .domain(d3.extent(data, d => d.survivors).map((d, i) => d * 1.05 * i))
         .range([height, 0])
 
+    const yTemperature = d3
+        .scaleLinear()
+        .domain(d3.extent(temperatures, d => d['temp C']).map((d, i) => d * [1.2, 1][i]))
+        .range([height, 0])
+
+    const lineColours = d3
+        .scaleOrdinal()
+        .range(['#54A24B', '#F58518', '#B279A2'])
+
+    // Temperatures
+    const area = d3
+        .area()
+        .x(d => x(d.long))
+        .y0(yTemperature(0))
+        .y1(d => yTemperature(d['temp C']))
+
     chart
         .append('path')
-        .datum(data)
+        .datum(temperatures)
+        .attr('fill', '#cbd5e1')
+        .attr('stroke', '#cbd5e1')
+        .attr('stroke-width', 1)
+        .attr('fill-opacity', 0.5)
+        .attr('d', d => area(d))
+
+    // Survivors
+    const line = d3
+        .line()
+        .x(d => x(d.long))
+        .y(d => ySurvivors(d.survivors))
+
+    chart
+        .selectAll('.drewLine')
+        .data(dataPerGroup)
+        .join('path')
         .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
+        .attr('stroke', d => lineColours(d[0]))
         .attr('stroke-width', 1.5)
-        .attr('d', d3.line()
-            .x(function (d) { return x(d.long) })
-            .y(function (d) { return y(d.survivors) })
+        .attr('d', d => line(d[1]))
+
+    addAxis(chart, height, width, marginDefault, x, ySurvivors, 'Longitude', 'Survivors', colours.text, undefined, d3.format('.1s'))
+
+    const yAxisRight = chart
+        .append('g')
+        .attr('transform', `translate(${width}, 0)`)
+        .call(
+            d3
+                .axisRight(yTemperature)
+                .tickSize(0)
+                .tickPadding(10)
+                .tickFormat(d => `${d} °C`)
         )
 
-    addAxis(chart, height, width, marginDefault, x, y, 'Longitude', 'Survivors', colours.text, undefined, d3.format('.1s'))
+    yAxisRight
+        .append('text')
+        .attr('x', ((height - marginDefault.bottom) / 2))
+        .attr('y', -55)
+        .attr('font-size', 12)
+        .attr('transform', 'rotate(90)')
+        .attr('text-anchor', 'middle')
+        .text('Temperature')
 
+    yAxisRight
+        .select('.domain')
+        .attr('stroke', colours.text)
+
+    yAxisRight
+        .selectAll('text')
+        .attr('fill', colours.text)
 })

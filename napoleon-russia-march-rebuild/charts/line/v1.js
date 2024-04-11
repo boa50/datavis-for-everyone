ed32528edb13d60b93598f79519b7f9f9de1463a
boa50 <bruno.albuquerque50@gmail.com>
2024-04-11 11:54:02 -0300

    Created the first version of tooltips for line charts

--- napoleon-russia-march-rebuild/charts/line/v1.js
+++ napoleon-russia-march-rebuild/charts/line/v1.js
@@ -1,4 +1,5 @@
-import { adjustColours } from "../axis.js"
+import { adjustColours } from '../axis.js'
+import { buildTooltip } from '../../../components/tooltip/script.js'
 
 export const lineV1 = (chart, width, height, dataPerGroup, temperatures, x, ySurvivors, yTemperature, marginBottom, axisColour) => {
     // Temperatures
@@ -8,14 +9,42 @@ export const lineV1 = (chart, width, height, dataPerGroup, temperatures, x, ySur
         .y0(yTemperature(0))
         .y1(d => yTemperature(d['temp C']))
 
+    const temperatureColour = '#cbd5e1'
+
     chart
         .append('path')
         .datum(temperatures)
-        .attr('fill', '#cbd5e1')
-        .attr('stroke', '#cbd5e1')
+        .attr('fill', temperatureColour)
+        .attr('stroke', temperatureColour)
         .attr('stroke-width', 1)
         .attr('fill-opacity', 0.5)
         .attr('d', d => area(d))
+
+    const { mouseover, mousemove, mouseleave } = buildTooltip('line-v1-container', (d) => `Temperature: ${d['temp C']}°C`)
+    const customMouseOver = function (event, d) {
+        d3.select(this).attr('fill', temperatureColour)
+        mouseover(event, d)
+    }
+    const customMouseLeave = function (event, d) {
+        d3.select(this).attr('fill', 'transparent')
+        mouseleave(event, d)
+    }
+
+    chart
+        .append('g')
+        .selectAll('.dot')
+        .data(temperatures)
+        .join('circle')
+        .attr('cx', d => x(d.long))
+        .attr('cy', d => yTemperature(d['temp C']))
+        .attr('r', 4)
+        .attr('stroke', 'transparent')
+        .attr('stroke-width', 12)
+        .attr('fill', 'transparent')
+        .on('mouseover', customMouseOver)
+        .on('mousemove', mousemove)
+        .on('mouseleave', customMouseLeave)
+
 
     // Survivors
     const lineColours = d3
@@ -35,6 +64,22 @@ export const lineV1 = (chart, width, height, dataPerGroup, temperatures, x, ySur
         .attr('stroke', d => lineColours(d[0]))
         .attr('stroke-width', 3)
         .attr('d', d => line(d[1]))
+
+    chart
+        .append('g')
+        .selectAll('.dot')
+        .data(dataPerGroup)
+        .join('circle')
+        .attr('class', 'crazydot')
+        .attr('cx', d => { console.log(d); return x(d.long) })
+        .attr('cy', d => ySurvivors(d.survivors))
+        .attr('r', 4)
+        .attr('stroke', 'transparent')
+        .attr('stroke-width', 12)
+        .attr('fill', 'transparent')
+        .on('mouseover', customMouseOver)
+        .on('mousemove', mousemove)
+        .on('mouseleave', customMouseLeave)
 
     chart
         .append('g')

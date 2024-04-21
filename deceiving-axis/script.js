6368d28b2d333ff322253dda80f5b2c66e2a673a
boa50 <bruno.albuquerque50@gmail.com>
2024-04-21 18:28:36 -0300

    Started reorganising the charts code

--- deceiving-axis/script.js
+++ deceiving-axis/script.js
@@ -19,8 +19,8 @@ const getData = () =>
             }))
     ])
 
-const svgWidth = 1080
-const svgHeight = 720
+const svgWidth = Math.floor(window.innerWidth / 2)
+const svgHeight = Math.floor(window.innerHeight / 2)
 const margin = {
     left: 64,
     right: 72,
@@ -31,72 +31,27 @@ const margin = {
 const width = svgWidth - margin.left - margin.right
 const height = svgHeight - margin.top - margin.bottom
 
-const chart1 = d3
-    .select(`#chart1`)
+const getChart = id => d3
+    .select(`#chart${id}`)
     .attr('width', svgWidth)
     .attr('height', svgHeight)
     .append('g')
     .attr('transform', `translate(${[margin.left, margin.top]})`)
 
-const chart2 = d3
-    .select(`#chart2`)
-    .attr('width', svgWidth)
-    .attr('height', svgHeight)
-    .append('g')
-    .attr('transform', `translate(${[margin.left, margin.top]})`)
 
-const chart3 = d3
-    .select(`#chart3`)
-    .attr('width', svgWidth)
-    .attr('height', svgHeight)
-    .append('g')
-    .attr('transform', `translate(${[margin.left, margin.top]})`)
+const chart1 = getChart(1)
+const chart2 = getChart(2)
+const chart3 = getChart(3)
 
+
+let x1
+const chart1Axis = document.getElementById('chart1-axis')
 
 getData().then(datasets => {
     const salaries = datasets[0]
     const obesity = datasets[1]
 
-    //Chart 1
-    const groupedData1 = d3
-        .flatRollup(salaries, v => d3.median(v, x => x.Base_Salary), d => d.Department)
-        .sort((a, b) => a[1] - b[1])
 
-    const groupedDataFiltered1 = [
-        ...groupedData1.slice(0, 3),
-        ...groupedData1.slice(Math.floor(groupedData1.length / 2) - 1, Math.floor(groupedData1.length / 2) + 2),
-        ...groupedData1.slice(groupedData1.length - 4, groupedData1.length - 1)
-    ]
-
-    const x1 = d3
-        .scaleLog()
-        .domain([1, d3.max(groupedDataFiltered1, d => d[1])])
-        .range([0, width])
-
-    const y1 = d3
-        .scaleBand()
-        .domain(groupedDataFiltered1.map(d => d[0]))
-        .range([height, 0])
-        .padding(0.2)
-
-    chart1
-        .selectAll('.bars')
-        .data(groupedDataFiltered1)
-        .join('rect')
-        .attr('x', x1(0))
-        .attr('y', d => y1(d[0]))
-        .attr('width', d => x1(d[1]))
-        .attr('height', y1.bandwidth())
-        .attr('fill', '#69b3a2')
-
-    addAxis({
-        chart: chart1,
-        height: height,
-        width: width,
-        margin: margin,
-        x: x1,
-        y: y1
-    })
 
 
     //Chart 2

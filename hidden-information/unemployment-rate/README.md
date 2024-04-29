d42d2e1bdea2da47497f2d64a1d036494a808f3e
boa50 <bruno.albuquerque50@gmail.com>
2024-04-29 10:56:19 -0300

    Organised the data for the unemployment rate chart

---
+++ hidden-information/unemployment-rate/README.md
@@ -0,0 +1,6 @@
+Showing how unemployment rate is calculates and how it can be used in favor of governments
+
+Sources:
+ - https://courses.lumenlearning.com/wm-macroeconomics/chapter/calculating-the-unemployment-rate/#:~:text=We%20can%20calculate%20the%20unemployment,force%2C%20then%20multiplying%20by%20100.
+ - https://www.ibge.gov.br/explica/desemprego.php (in Portuguese)
+ - https://www.ibge.gov.br/estatisticas/sociais/trabalho/9173-pesquisa-nacional-por-amostra-de-domicilios-continua-trimestral.html?=&t=series-historicas (dataset, in Portuguese)
---
+++ hidden-information/unemployment-rate/data/etl.py
@@ -0,0 +1,48 @@
+### Delete the first column and row of the original file before executing this script
+from os import path
+import pandas as pd
+
+
+def get_path(file_name):
+    return path.join(path.dirname(path.realpath(__file__)), file_name)
+
+
+def get_month_number(month):
+    month_to_num_dict = {
+        "jan": "01",
+        "fev": "02",
+        "mar": "03",
+        "abr": "04",
+        "mai": "05",
+        "jun": "06",
+        "jul": "07",
+        "ago": "08",
+        "set": "09",
+        "out": "10",
+        "nov": "11",
+        "dez": "12",
+    }
+
+    return month_to_num_dict[month]
+
+
+df = pd.read_csv(get_path("unemployment_raw.csv"))
+
+df = df.transpose().reset_index()
+
+df.columns = ["dt_txt", "unemploymentRate"]
+
+df["year"] = df["dt_txt"].str.split(expand=True).iloc[:, 1]
+
+df["month"] = df["dt_txt"].str.split("-", expand=True).iloc[:, 0]
+
+df["month_num"] = df["month"].apply(get_month_number)
+
+df["isoDate"] = df["year"] + "-" + df["month_num"] + "-01"
+
+df = df[["isoDate", "unemploymentRate"]]
+
+print("Number of NaN")
+print(df.isna().sum())
+
+df.to_csv(get_path("unemployment.csv"), index=False)

139db9b745f560407685d6d355dea5cdde0b20a1
boa50 <bruno.albuquerque50@gmail.com>
2024-04-29 09:10:47 -0300

    Updated the chart of gdp and happines by year to reflect the changes in the list of countries

--- spurious-correlations/charts/gdp-&-happiness-by-year.js
+++ spurious-correlations/charts/gdp-&-happiness-by-year.js
@@ -3,150 +3,176 @@ import { addAxis } from "../../components/axis/script.js"
 import { addLegendV2 as addLegend } from "../../components/legend/script.js"
 import { formatCurrency } from "../../components/utils.js"
 
-const getData = () =>
-    d3.csv('./data/gdp-vs-happiness-cleansed.csv')
-        .then(d => d.map(v => {
-            return {
-                ...v,
-                gdpPerCapita: +v.gdpPerCapita,
-                lifeSatisfaction: +v.lifeSatisfaction
-            }
-        }))
+const drawChart = ({
+    data,
+    chart,
+    lineLifeSatisfaction,
+    lineGdp,
+    countries
+}) => {
+    const dataFiltered = data.filter(d => countries.includes(d.country))
+    const dataGrouped = d3.group(dataFiltered, d => d.country)
+    const colourLifeSatisfaction = d3
+        .scaleOrdinal()
+        .domain(countries)
+        .range(d3.schemeCategory10)
+    const colourGdp = d3
+        .scaleOrdinal()
+        .domain(countries)
+        .range(d3.schemeCategory10.map(d => d + '75'))
 
-export const plotChart = (chartProps, countries, countriesSelector) => {
-    const { chart, width, height, margin } = chartProps
+    const strokeWidth = 3
+    chart
+        .selectAll('.lifeSatisfactionLine')
+        .data(dataGrouped)
+        .join('path')
+        .attr('class', 'lifeSatisfactionLine')
+        .attr('fill', 'none')
+        .attr('stroke', d => colourLifeSatisfaction(d[0]))
+        .attr('stroke-width', strokeWidth)
+        .attr('d', d => lineLifeSatisfaction(d[1]))
 
-    getData().then(data => {
-        const allCountries = [...new Set(data.map(d => d.country))].sort()
-        countriesSelector.innerHTML = allCountries
-            .map(country => `<option value="${country}" ${countries.includes(country) ? 'selected' : null}>${country}</option>`)
-        countriesSelector.loadOptions()
+    chart
+        .selectAll('.gdpLine')
+        .data(dataGrouped)
+        .join('path')
+        .attr('class', 'gdpLine')
+        .attr('fill', 'none')
+        .attr('stroke', d => colourGdp(d[0]))
+        .attr('stroke-width', strokeWidth)
+        .attr('d', d => lineGdp(d[1]))
 
-        const gdpAxisTicks = [1, 2, 5, 15, 30, 60, 120].map(d => d * 1e3)
-
-        const x = d3
-            .scaleLinear()
-            .domain(d3.extent(data, d => d.year))
-            .range([0, width])
-
-        const yLeft = d3
-            .scaleLinear()
-            .domain([0, 10])
-            .range([height - 8, 0])
-
-        const yRight = d3
-            .scaleLog()
-            .domain(d3.extent(data, d => d.gdpPerCapita))
-            .range([height - 8, 0])
-
-        const lineLifeSatisfaction = d3
-            .line()
-            .x(d => x(d.year))
-            .y(d => yLeft(d.lifeSatisfaction))
-
-        const lineGdp = d3
-            .line()
-            .x(d => x(d.year))
-            .y(d => yRight(d.gdpPerCapita))
-
-        addAxis({
-            chart: chart,
-            height: height,
-            width: width,
-            x: x,
-            y: yLeft,
-            yRight: yRight,
-            xLabel: 'Year',
-            yLabel: 'Life satisfaction (0 - 10)',
-            yRightLabel: 'GDP per Capita',
-            xFormat: d3.format(''),
-            yFormat: d3.format('.1s'),
-            yRightFormat: d => `${formatCurrency(d / 1000)}k`,
-            colour: colours.axis,
-            hideYdomain: true,
-            yRightTickValues: gdpAxisTicks
-        })
-
-        const dataFiltered = data.filter(d => countries.includes(d.country))
-        const dataGrouped = d3.group(dataFiltered, d => d.country)
-        const colourLifeSatisfaction = d3
-            .scaleOrdinal()
-            .range(d3.schemeCategory10)
-        const colourGdp = d3
-            .scaleOrdinal()
-            .range(d3.schemeCategory10.map(d => d + '75'))
-
-        const strokeWidth = 3
-        chart
-            .selectAll('.drewLine')
-            .data(dataGrouped)
-            .join('path')
-            .attr('fill', 'none')
-            .attr('stroke', d => colourLifeSatisfaction(d[0]))
-            .attr('stroke-width', strokeWidth)
-            .attr('d', d => lineLifeSatisfaction(d[1]))
-
-        chart
-            .selectAll('.drewLine')
-            .data(dataGrouped)
-            .join('path')
-            .attr('fill', 'none')
-            .attr('stroke', d => colourGdp(d[0]))
-            .attr('stroke-width', strokeWidth)
-            .attr('d', d => lineGdp(d[1]))
-
-
-        // addLineTooltip(
-        //     'charts',
-        //     d => `
-        //     <div style="display: flex; justify-content: space-between">
-        //         <span>Year:&emsp;</span>
-        //         <span>${d.year}</span>
-        //     </div>
-        //     <div style="display: flex; justify-content: space-between">
-        //         <span>Degrees Awarded:&emsp;</span>
-        //         <span>${d.awards}</span>
-        //     </div>
-        //     `,
-        //     colours.line1,
-        //     {
-        //         chart: chart,
-        //         data: data,
-        //         cx: d => x(d.year),
-        //         cy: d => yLeft(d.awards),
-        //         radius: 5
-        //     }
-        // )
-
-        // addLineTooltip(
-        //     'charts',
-        //     d => `
-        //     <div style="display: flex; justify-content: space-between">
-        //         <span>Year:&emsp;</span>
-        //         <span>${d.year}</span>
-        //     </div>
-        //     <div style="display: flex; justify-content: space-between">
-        //         <span>Search Volume:&emsp;</span>
-        //         <span>${d3.format('.1f')(d.searches)}</span>
-        //     </div>
-        //     `,
-        //     colours.line2,
-        //     {
-        //         chart: chart,
-        //         data: data,
-        //         cx: d => x(d.year),
-        //         cy: d => yRight(d.searches),
-        //         radius: 5
-        //     }
-        // )
-
-        addLegend({
-            chart: chart,
-            legends: ['Life satisfaction', 'GDP per capita'],
-            colours: [colourLifeSatisfaction(countries[0]), colourGdp(countries[0])],
-            xPos: -64,
-            yPos: -32
-        })
-
+    addLegend({
+        chart: chart,
+        legends: ['Life satisfaction', 'GDP per capita'],
+        colours: [colourLifeSatisfaction(countries[0]), colourGdp(countries[0])],
+        xPos: -64,
+        yPos: -32
     })
+}
+
+
+export const updateChart = ({
+    data,
+    chart,
+    lineLifeSatisfaction,
+    lineGdp,
+    countries
+}) => {
+    const legendId = chart.attr('id') + '-legend'
+    chart.select(`#${legendId}`).remove()
+
+    drawChart({
+        data,
+        chart,
+        lineLifeSatisfaction,
+        lineGdp,
+        countries
+    })
+}
+
+export const plotChart = (data, chartProps, countries) => {
+    const { chart, width, height } = chartProps
+
+    const gdpAxisTicks = [1, 2, 5, 15, 30, 60, 120].map(d => d * 1e3)
+
+    const x = d3
+        .scaleLinear()
+        .domain(d3.extent(data, d => d.year))
+        .range([0, width])
+
+    const yLeft = d3
+        .scaleLinear()
+        .domain([0, 10])
+        .range([height - 8, 0])
+
+    const yRight = d3
+        .scaleLog()
+        .domain(d3.extent(data, d => d.gdpPerCapita))
+        .range([height - 8, 0])
+
+    const lineLifeSatisfaction = d3
+        .line()
+        .x(d => x(d.year))
+        .y(d => yLeft(d.lifeSatisfaction))
+
+    const lineGdp = d3
+        .line()
+        .x(d => x(d.year))
+        .y(d => yRight(d.gdpPerCapita))
+
+    addAxis({
+        chart: chart,
+        height: height,
+        width: width,
+        x: x,
+        y: yLeft,
+        yRight: yRight,
+        xLabel: 'Year',
+        yLabel: 'Life satisfaction (0 - 10)',
+        yRightLabel: 'GDP per Capita',
+        xFormat: d3.format(''),
+        yFormat: d3.format('.1s'),
+        yRightFormat: d => `${formatCurrency(d / 1000)}k`,
+        colour: colours.axis,
+        hideYdomain: true,
+        yRightTickValues: gdpAxisTicks
+    })
+
+    const chartObject = {
+        data,
+        chart,
+        lineLifeSatisfaction,
+        lineGdp,
+        countries
+    }
+
+    drawChart(chartObject)
+
+    return chartObject
+
+
+    // addLineTooltip(
+    //     'charts',
+    //     d => `
+    //     <div style="display: flex; justify-content: space-between">
+    //         <span>Year:&emsp;</span>
+    //         <span>${d.year}</span>
+    //     </div>
+    //     <div style="display: flex; justify-content: space-between">
+    //         <span>Degrees Awarded:&emsp;</span>
+    //         <span>${d.awards}</span>
+    //     </div>
+    //     `,
+    //     colours.line1,
+    //     {
+    //         chart: chart,
+    //         data: data,
+    //         cx: d => x(d.year),
+    //         cy: d => yLeft(d.awards),
+    //         radius: 5
+    //     }
+    // )
+
+    // addLineTooltip(
+    //     'charts',
+    //     d => `
+    //     <div style="display: flex; justify-content: space-between">
+    //         <span>Year:&emsp;</span>
+    //         <span>${d.year}</span>
+    //     </div>
+    //     <div style="display: flex; justify-content: space-between">
+    //         <span>Search Volume:&emsp;</span>
+    //         <span>${d3.format('.1f')(d.searches)}</span>
+    //     </div>
+    //     `,
+    //     colours.line2,
+    //     {
+    //         chart: chart,
+    //         data: data,
+    //         cx: d => x(d.year),
+    //         cy: d => yRight(d.searches),
+    //         radius: 5
+    //     }
+    // )
 }
--- spurious-correlations/script.js
+++ spurious-correlations/script.js
@@ -1,7 +1,17 @@
 import { getChart } from "../components/utils.js";
 import { plotChart as plotAwardsBySearches } from "./charts/awards-by-searches.js";
 import { plotChart as plotGdpVsHappiness } from "./charts/gdp-vs-happiness.js";
-import { plotChart as plotGdpHappinessByYear } from "./charts/gdp-&-happiness-by-year.js";
+import { plotChart as plotGdpHappinessByYear, updateChart as updateGdpHappinessByYear } from "./charts/gdp-&-happiness-by-year.js";
+
+const getData = () =>
+    d3.csv('./data/gdp-vs-happiness-cleansed.csv')
+        .then(d => d.map(v => {
+            return {
+                ...v,
+                gdpPerCapita: +v.gdpPerCapita,
+                lifeSatisfaction: +v.lifeSatisfaction
+            }
+        }))
 
 const svgHeight = window.innerHeight / 2
 
@@ -49,8 +59,8 @@ plotGdpVsHappiness(
 const countriesSelector = document.getElementById('chart4-countries')
 const countries = ['China']
 
-plotGdpHappinessByYear(
-    getChart(
+getData().then(gdpVsHappinesData => {
+    const gdpHappinessByYearChart = getChart(
         'chart4',
         document.getElementById('chart4-container').offsetWidth,
         svgHeight,
@@ -60,7 +70,32 @@ plotGdpHappinessByYear(
             top: 32,
             bottom: 56
         }
-    ),
-    countries,
-    countriesSelector
-)
+    )
+
+    const gdpHappinessByYearObject = plotGdpHappinessByYear(
+        gdpVsHappinesData,
+        gdpHappinessByYearChart,
+        countries
+    )
+
+    // Filling the select box with all countries names
+    const allCountries = [...new Set(gdpVsHappinesData.map(d => d.country))].sort()
+    countriesSelector.innerHTML = allCountries
+        .map(country => `<option value="${country}" ${countries.includes(country) ? 'selected' : null}>${country}</option>`)
+    countriesSelector.loadOptions()
+
+    // Updating the chart when the list of selected countries change
+    countriesSelector.addEventListener('change', function (event) {
+        for (let option of event.target.options) {
+            if (option.selected
+                && !countries.includes(option.value))
+                countries.push(option.value)
+
+            if (!option.selected
+                && countries.includes(option.value))
+                countries.splice(countries.indexOf(option.value), 1)
+        }
+
+        updateGdpHappinessByYear({ ...gdpHappinessByYearObject, countries: countries })
+    })
+})

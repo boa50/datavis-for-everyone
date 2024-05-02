8ba844a4502f1c8994da70b4c5bda351f97bf4a3
boa50 <bruno.albuquerque50@gmail.com>
2024-05-02 11:28:42 -0300

    Cleansed the life expectancy data to be used by the engaging charts dashboard

---
+++ engaging-charts/README.md
@@ -0,0 +1,4 @@
+Showing how creating engaging charts can draw more people's attention
+
+Data sources:
+ - life-expectation-at-birth-by-sex.csv (https://ourworldindata.org/grapher/life-expectation-at-birth-by-sex)
---
+++ engaging-charts/data/etl.py
@@ -0,0 +1,24 @@
+from os import path
+import pandas as pd
+
+
+# Get file complete path
+def get_path(file_name):
+    return path.join(path.dirname(path.realpath(__file__)), file_name)
+
+
+df = pd.read_csv(
+    get_path("life-expectation-at-birth-by-sex.csv"),
+    names=["country", "code", "year", "female", "male"],
+    skiprows=1,
+)
+
+df = df[(df["year"] == 2021) & (df["code"].notnull())]
+
+df = df[["country", "female", "male"]]
+
+print(df.head())
+print(df.isnull().sum())
+
+
+df.to_csv(get_path("life-expectancy.csv"), index=False)

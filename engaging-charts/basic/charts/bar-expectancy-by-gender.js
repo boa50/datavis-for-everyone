4745a34b3b38a03420c87fa19a5cad305a9c4a5d
boa50 <bruno.albuquerque50@gmail.com>
2024-05-02 12:04:16 -0300

    Added a grouped bar chart to the life expectancy chart

--- engaging-charts/basic/charts/bar-expectancy-by-gender.js
+++ engaging-charts/basic/charts/bar-expectancy-by-gender.js
@@ -1,8 +1,11 @@
 export const addChart = (chartProps, data) => {
     const { chart, width, height } = chartProps
 
-    const filteredData = d3.sort(data, (a, b) => d3.descending(a.average, b.average)).slice(0, 10)
+    const filteredData = d3.sort(data, (a, b) => d3.descending(a.average, b.average)).slice(0, 2)
     const maxLifeExpectancy = Math.max(d3.max(filteredData, d => d.female), d3.max(data, d => d.male))
+    const groups = filteredData.map(d => d.country)
+    const subgroups = ['female', 'male']
+    const getSubgroupValues = d => subgroups.map(subgroup => { return { subgroup: subgroup, value: d[subgroup] } })
 
     const x = d3
         .scaleLinear()
@@ -11,17 +14,27 @@ export const addChart = (chartProps, data) => {
 
     const y = d3
         .scaleBand()
-        .domain(filteredData.map(d => d.country))
+        .domain(groups)
         .range([0, height])
         .padding(.1)
 
+    const ySubgroup = d3
+        .scaleBand()
+        .domain(subgroups)
+        .range([0, y.bandwidth()])
+        .padding(.05)
+
     chart
-        .selectAll('myRect')
+        .selectAll('g')
         .data(filteredData)
+        .join('g')
+        .attr('transform', d => `translate(0, ${y(d.country)})`)
+        .selectAll('rect')
+        .data(getSubgroupValues)
         .join('rect')
         .attr('x', x(0))
-        .attr('y', d => y(d.country))
-        .attr('width', d => x(d.female))
-        .attr('height', y.bandwidth())
+        .attr('y', d => ySubgroup(d.subgroup))
+        .attr('width', d => x(d.value))
+        .attr('height', ySubgroup.bandwidth())
         .attr('fill', '#69b3a2')
 }
--- snippets/charts/bar.js
+++ snippets/charts/bar.js
@@ -1,20 +1,57 @@
-const x = d3
-    .scaleLinear()
-    .domain([0, MAX_VALUE])
-    .range([0, width])
+const HORIZONTAL_BARS = () => {
+    const x = d3
+        .scaleLinear()
+        .domain([0, MAX_VALUE])
+        .range([0, width])
 
-const y = d3
-    .scaleBand()
-    .domain(data.map(d => d.GROUP_FIELD))
-    .range([0, height])
-    .padding(.1)
+    const y = d3
+        .scaleBand()
+        .domain(data.map(d => d.GROUP_FIELD))
+        .range([0, height])
+        .padding(.1)
 
-chart
-    .selectAll('myRect')
-    .data(data)
-    .join('rect')
-    .attr('x', x(0))
-    .attr('y', d => y(d.GROUP_FIELD))
-    .attr('width', d => x(d.VALUE))
-    .attr('height', y.bandwidth())
-    .attr('fill', '#69b3a2')
+    chart
+        .selectAll('.myRect')
+        .data(data)
+        .join('rect')
+        .attr('x', x(0))
+        .attr('y', d => y(d.GROUP_FIELD))
+        .attr('width', d => x(d.VALUE))
+        .attr('height', y.bandwidth())
+        .attr('fill', '#69b3a2')
+}
+
+const GROUPED_HORIZONTAL_BARS = () => {
+    const getSubgroupValues = d => SUBGROUPS.map(subgroup => { return { subgroup: subgroup, value: d[subgroup] } })
+
+    const x = d3
+        .scaleLinear()
+        .domain([0, MAX_VALUE])
+        .range([0, width])
+
+    const y = d3
+        .scaleBand()
+        .domain(GROUPS)
+        .range([0, height])
+        .padding(.1)
+
+    const ySubgroup = d3
+        .scaleBand()
+        .domain(SUBGROUPS)
+        .range([0, y.bandwidth()])
+        .padding(.05)
+
+    chart
+        .selectAll('g')
+        .data(data)
+        .join('g')
+        .attr('transform', d => `translate(0, ${y(d.GROUP_FIELD)})`)
+        .selectAll('rect')
+        .data(getSubgroupValues)
+        .join('rect')
+        .attr('x', x(0))
+        .attr('y', d => ySubgroup(d.subgroup))
+        .attr('width', d => x(d.value))
+        .attr('height', ySubgroup.bandwidth())
+        .attr('fill', '#69b3a2')
+}

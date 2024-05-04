12bf5a4f118f6bb3981c0afc07dafe885c9e0aff
boa50 <bruno.albuquerque50@gmail.com>
2024-05-04 15:55:03 -0300

    Moved my code snippets to a different project

--- snippets/titles.js
+++
@@ -1,9 +0,0 @@
-`
-<h1 id="title" class="title font-sans text-3xl text-gray-700">${PAGE_TITLE}</h1>
-<h2 class="subtitle font-sans text-base text-gray-500">${PAGE_SUBTITLE}</h2>
-`
-
-    `
-<h3 class="font-sans text-xl text-gray-700">${CHART_TITLE}</h3>
-<h4 class="font-sans text-sm text-gray-400">${CHART_SUBTITLE}</h4>
-`
--- snippets/tooltip.js
+++
@@ -1,7 +0,0 @@
-`
-<strong>${TITLE}</strong>   
-<div style="display: flex; justify-content: space-between">
-    <span>FIELD_NAME:&emsp;</span>
-    <span>${FIELD_VALUE}</span>
-</div>
-`

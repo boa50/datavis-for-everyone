381092802391354ca6918f4c776f4ec0d9b55a61
boa50 <bruno.albuquerque50@gmail.com>
2024-05-06 10:33:38 -0300

    Cleansed the greenhouse gases emissions dataset

--- engaging-charts/README.md
+++ engaging-charts/README.md
@@ -2,3 +2,4 @@ Showing how creating engaging charts can draw more people's attention
 
 Data sources:
  - life-expectation-at-birth-by-sex.csv (https://ourworldindata.org/grapher/life-expectation-at-birth-by-sex)
+ - ghg-emissions-by-sector.csv (https://ourworldindata.org/grapher/ghg-emissions-by-sector)
--- engaging-charts/data/etl.py
+++ engaging-charts/data/etl.py
@@ -7,6 +7,7 @@ def get_path(file_name):
     return path.join(path.dirname(path.realpath(__file__)), file_name)
 
 
+### Life expectancy data
 df = pd.read_csv(
     get_path("life-expectation-at-birth-by-sex.csv"),
     names=["country", "code", "year", "female", "male"],
@@ -20,5 +21,47 @@ df = df[["year", "country", "female", "male"]]
 print(df.head())
 print(df.isnull().sum())
 
-
 df.to_csv(get_path("life-expectancy.csv"), index=False)
+
+
+### Greenhouse gases data
+df = pd.read_csv(
+    get_path("ghg-emissions-by-sector.csv"),
+    names=[
+        "country",
+        "code",
+        "year",
+        "agriculture",
+        "landUseChange",
+        "waste",
+        "buildings",
+        "industry",
+        "manufacturingAndConstruction",
+        "transport",
+        "electricityAndHeat",
+        "energyProduction",
+        "otherFuelCombustion",
+        "bunkerFuels",
+    ],
+    skiprows=1,
+)
+
+df = df[(df["year"] >= 2000) & (df["country"] == "World")]
+
+df = df[
+    [
+        "country",
+        "year",
+        "agriculture",
+        "buildings",
+        "industry",
+        "manufacturingAndConstruction",
+        "transport",
+        "electricityAndHeat",
+    ]
+]
+
+print(df.head())
+print(df.isnull().sum())
+
+df.to_csv(get_path("greenhouse-emissions.csv"), index=False)

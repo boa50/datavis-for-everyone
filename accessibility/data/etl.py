7ee4852aa0fda19dd9896a1af2d1bb0585817749
boa50 <bruno.albuquerque50@gmail.com>
2024-05-15 09:48:31 -0300

    Reorganised the data to make it easier to work with while creating charts

--- accessibility/data/etl.py
+++ accessibility/data/etl.py
@@ -11,12 +11,14 @@ df = pd.read_csv(
     usecols=["Entity", "Year", "2023", "2022", "2021", "2020", "2019"],
 )
 
-df.columns = ["country", "month", "2023", "2022", "2021", "2020", "2019"]
+df = df[df["Entity"] == "World"]
 
-df = df[df["country"] == "World"]
+df = df.drop("Entity", axis=1).reset_index(drop=True)
 
-df = df.drop("country", axis=1).reset_index(drop=True)
+df = pd.melt(df, id_vars="Year")
 
-print(df)
+df.columns = ["month", "year", "temperature"]
+
+print(df.head())
 
 df.to_csv(get_path("average-surface-temperature.csv"), index=False)

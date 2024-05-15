80176379b2b838ace6331f4148b882c4038583c4
boa50 <bruno.albuquerque50@gmail.com>
2024-05-15 09:31:12 -0300

    Created a standard palette to my charts

---
+++ colours.js
@@ -0,0 +1,10 @@
+// Pallete based on https://jfly.uni-koeln.de/color/
+export const palette = {
+    orange: '#E69F00',
+    skyBlue: '#56B4E9',
+    bluishGreen: '#009E73',
+    amber: '#F5C710',
+    blue: '#0072B2',
+    vermillion: '#D55E00',
+    reddishPurple: '#CC79A7'
+}

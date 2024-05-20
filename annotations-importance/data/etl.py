c240af332a419c82d480edcb32d2b490d9327808
boa50 <bruno.albuquerque50@gmail.com>
2024-05-20 11:42:43 -0300

    Organised the dataset to be used to create the chart

---
+++ annotations-importance/data/etl.py
@@ -0,0 +1,42 @@
+from os import path
+import pandas as pd
+
+
+def get_path(file_name):
+    return path.join(path.dirname(path.realpath(__file__)), file_name)
+
+
+df = pd.read_csv(
+    get_path("number-of-deaths-from-natural-disasters.csv"),
+    usecols=[
+        "Entity",
+        "Year",
+        "Total deaths - Drought",
+        "Total deaths - Flood",
+        "Total deaths - Earthquake",
+        "Total deaths - Extreme weather",
+        "Total deaths - Extreme temperature",
+        "Total deaths - Volcanic activity",
+    ],
+)
+
+df = df[df["Entity"] == "World"]
+df = df.drop("Entity", axis=1)
+df = df.sort_values(by="Year")
+df = df.reset_index(drop=True)
+df = df.fillna(0)
+df = df.astype("int64")
+
+df.columns = [
+    "year",
+    "drought",
+    "flood",
+    "earthquake",
+    "weather",
+    "temperature",
+    "volcanic",
+]
+
+print(df)
+
+df.to_csv(get_path("disasters-deaths.csv"), index=False)

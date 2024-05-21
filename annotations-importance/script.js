fb123e354bb8c2af16a0332b04e1b189d92a5ad3
boa50 <bruno.albuquerque50@gmail.com>
2024-05-21 11:28:16 -0300

    Added text explanations to the chart

--- annotations-importance/script.js
+++ annotations-importance/script.js
@@ -12,7 +12,7 @@ let visualisationsWidth
 let windowHeight
 let explanationText
 
-const nSteps = 3
+const nSteps = 9
 const stepsSizes = {}
 const eventSteps = new Set([])
 for (let i = 0; i < nSteps; i++) {
@@ -60,12 +60,36 @@ const handleStepProgress = (response) => {
             break;
         case 1:
             changeText(explanationText, `
-                Some text
+                Without any explanation text it is very hard to gain meaningful insights quickly
             `)
             showHideExplanationText({ executeShow: false })
             break;
         case 2:
-            changeText(explanationText, 'Another text')
+            changeText(explanationText, 'Let\'s add the basics: title and subtitle')
+            showHideExplanationText()
+            break;
+        case 3:
+            changeText(explanationText, 'With those now we at least have an idea about what was the intention')
+            showHideExplanationText()
+            break;
+        case 4:
+            changeText(explanationText, 'Let\'s put more annotations to individual data points')
+            showHideExplanationText()
+            break;
+        case 5:
+            changeText(explanationText, 'Now things are more explicit, but our chart has a lot of information disturbing our analysis')
+            showHideExplanationText()
+            break;
+        case 6:
+            changeText(explanationText, 'So, let\'s remove the excesses <span style="color: transparent;">and add only meaningful texts<span>')
+            showHideExplanationText({ executeHide: false })
+            break;
+        case 7:
+            changeText(explanationText, 'So, let\'s remove the excesses <span style="color: inherit;">and add only meaningful texts<span>')
+            showHideExplanationText({ executeShow: false })
+            break;
+        case 8:
+            changeText(explanationText, 'Now we have only what is essential to share insights quickly with our audience and we can enhance our understanding about the data')
             showHideExplanationText()
             break;
 

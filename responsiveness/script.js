4a9f058db2362d6caaf5fadadb1b5ac6c61bd645
boa50 <bruno.albuquerque50@gmail.com>
2024-06-27 14:37:41 -0300

    Changed the function to append chart containers to the one used in the visual-components package

--- responsiveness/script.js
+++ responsiveness/script.js
@@ -1,4 +1,4 @@
-import { getChart, getMargin } from "../node_modules/visual-components/index.js"
+import { getChart, getMargin, appendChartContainer } from "../node_modules/visual-components/index.js"
 import { addChart as addBar } from "./charts/bar-expectancy-by-gender.js"
 import { addChart as addLine } from "./charts/line-life-expectancy-gap.js"
 import { addChart as addScatter } from "./charts/scatter-expectancy-distribution.js"
@@ -15,42 +15,15 @@ const getData = () =>
             }
         }))
 
-const addChartContainer = (idNum, chartTitle = '') => {
-    const chartId = `chart${idNum}`
-
-    d3
-        .select('#charts')
-        .append('div')
-        .attr('class', 'bg-neutral-50 px-4 py-2 rounded-sm')
-        .append('div')
-        .attr('id', `${chartId}-container`)
-        .attr('class', 'aspect-[4/3] md:aspect-video')
-        .call(g =>
-            g
-                .append('h3')
-                .attr('id', `${chartId}-title`)
-                .attr('class', 'text-sm md:text-base text-gray-700 font-medium')
-                .text(chartTitle)
-        )
-        .call(g =>
-            g
-                .append('svg')
-                .attr('id', chartId)
-        )
-
-    return chartId
-}
-
-const barChartId = addChartContainer(1, 'Life expectancy by Country by Gender')
-const lineChartId = addChartContainer(2, 'Life expectancy gap by Country per Year')
-const scatterChartId = addChartContainer(3, 'Life expectancy distribution')
+const barChartId = appendChartContainer({ idNum: 1, chartTitle: 'Life expectancy by Country by Gender' })
+const lineChartId = appendChartContainer({ idNum: 2, chartTitle: 'Life expectancy gap by Country per Year' })
+const scatterChartId = appendChartContainer({ idNum: 3, chartTitle: 'Life expectancy distribution' })
 
 getData().then(data => {
     addBar(
         getChart({
             id: barChartId,
-            margin: getMargin({ left: 140, top: 24 }),
-            chartDimensions: {}
+            margin: getMargin({ left: 140, top: 24 })
         }),
         data.filter(d => d.year === '2021')
     )
@@ -58,16 +31,14 @@ getData().then(data => {
     addLine(
         getChart({
             id: lineChartId,
-            margin: getMargin({ top: 24 }),
-            chartDimensions: {}
+            margin: getMargin({ top: 24 })
         }),
         data
     )
 
     addScatter(
         getChart({
-            id: scatterChartId,
-            chartDimensions: {}
+            id: scatterChartId
         }),
         data
     )

527539ed3e19b76ac39da1b49a622630aca36005
boa50 <bruno.albuquerque50@gmail.com>
2024-07-09 14:27:20 -0300

    Added the chart aggregated by economy situation

--- simplicity/script.js
+++ simplicity/script.js
@@ -10,7 +10,7 @@ const allColouredId = appendChartContainer({ idNum: 1, chartTitle: 'Everything c
 const allSingleFocusedId = appendChartContainer({ idNum: 2, chartTitle: 'Focusing only on one line and the others gray' })
 const chosenCountriesId = appendChartContainer({ idNum: 3, chartTitle: 'A small number of countries selected and the one in focus' })
 const aggRegionId = appendChartContainer({ idNum: 4, chartTitle: 'A country in focus and the others as an aggregation by region' })
-appendChartContainer({ idNum: 5, chartTitle: 'A country in focus and the others as an aggregation by economy' })
+const aggEconomyId = appendChartContainer({ idNum: 5, chartTitle: 'A country in focus and the others as an aggregation by economy' })
 appendChartContainer({ idNum: 6, chartTitle: 'Put the labels on the lines' })
 appendChartContainer({ idNum: 7, chartTitle: 'Add a message to specific data points' })
 
@@ -54,6 +54,16 @@ getData().then(data => {
             margin: dafaultMargin
         }),
         data.filter(d => ['Croatia', 'Europe and Central Asia (WB)', 'Latin America and Caribbean (WB)', 'Middle East and North Africa (WB)', 'North America (WB)', 'South Asia (WB)', 'Sub-Saharan Africa (WB)'].includes(d.entity)),
+        'Croatia'
+    )
+
+    addChart(
+        getChart({
+            id: aggEconomyId,
+            chartDimensions: getChartDimensions({ chartId: aggEconomyId }),
+            margin: dafaultMargin
+        }),
+        data.filter(d => ['Croatia', 'High-income countries', 'Low-income countries', 'Lower-middle-income countries', 'Middle-income countries', 'Upper-middle-income countries'].includes(d.entity)),
         'Croatia'
     )
 })

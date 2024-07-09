a9f8b6aa58e06052e4827f4b31e7e6c8e21a0842
boa50 <bruno.albuquerque50@gmail.com>
2024-07-09 11:17:57 -0300

    Organised the data for the charts

---
+++ simplicity/README.md
@@ -0,0 +1,4 @@
+Showing ways to simplify charts to make them more appealing
+
+Sources:
+ - gdp-per-capita-worldbank.csv (https://ourworldindata.org/grapher/gdp-per-capita-worldbank)
---
+++ simplicity/data/etl.py
@@ -0,0 +1,37 @@
+from os import path
+import pandas as pd
+import numpy as np
+
+
+def get_path(file_name):
+    return path.join(path.dirname(path.realpath(__file__)), file_name)
+
+
+df = pd.read_csv(get_path("gdp-per-capita-worldbank.csv"))
+
+df = df.drop("Code", axis=1)
+
+df.columns = ["entity", "year", "gdpPerCapita"]
+
+df = df[(df["year"] >= 2000) & (df["entity"] != "World")]
+
+aggregated_entities = [
+    "Europe and Central Asia (WB)",
+    "European Union (27)",
+    "Latin America and Caribbean (WB)",
+    "Low-income countries",
+    "Lower-middle-income countries",
+    "Middle East and North Africa (WB)",
+    "Middle-income countries",
+    "North America (WB)",
+    "South Asia (WB)",
+    "Sub-Saharan Africa (WB)",
+    "Upper-middle-income countries",
+]
+
+df["group"] = np.where(df["entity"].isin(aggregated_entities), "aggregated", "country")
+
+print(df.isna().sum())
+print(df.head())
+
+df.to_csv(get_path("gdp-per-capita.csv"), index=False)

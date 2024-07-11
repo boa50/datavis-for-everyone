db93a43aa222a057b7a47ad8524c2fe4a2b0eca0
boa50 <bruno.albuquerque50@gmail.com>
2024-07-11 14:15:16 -0300

    Created the bar chart

---
+++ light-dark-background/charts/bar.js
@@ -0,0 +1,42 @@
+import { addAxis } from "../../node_modules/visual-components/index.js"
+import { palette } from "../../colours.js"
+
+export const addChart = (chartProps, data) => {
+    const { chart, width, height } = chartProps
+    const barColour = palette.blue
+    const axesColour = palette.axis
+
+    const x = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.average) * 1.05])
+        .range([0, width])
+
+    const y = d3
+        .scaleBand()
+        .domain(data.map(d => d.country))
+        .range([0, height])
+        .padding(.25)
+
+    chart
+        .selectAll('.myRect')
+        .data(data)
+        .join('rect')
+        .attr('x', x(0))
+        .attr('y', d => y(d.country))
+        .attr('width', d => x(d.average))
+        .attr('height', y.bandwidth())
+        .attr('fill', barColour)
+
+    addAxis({
+        chart,
+        height,
+        width,
+        colour: axesColour,
+        x,
+        y,
+        xLabel: 'Average life expectancy',
+        yLabel: 'Country',
+        hideXdomain: true,
+        hideYdomain: true
+    })
+}
--- light-dark-background/script.js
+++ light-dark-background/script.js
@@ -1,10 +1,18 @@
 import { getChart, getMargin, getChartDimensions, appendChartContainer } from "../node_modules/visual-components/index.js"
+import { addChart as addBar } from "./charts/bar.js"
 
 const getData = () =>
     d3.csv('../_data/life-expectancy.csv')
-        .then(d => d.map(v => { return { ...v, male: +v.male, female: +v.female } }))
+        .then(d =>
+            d
+                .map(v => {
+                    return {
+                        ...v, male: +v.male, female: +v.female, average: (+v.male + +v.female) / 2
+                    }
+                })
+        )
 
-appendChartContainer({ idNum: 1, chartTitle: 'All Light - Bar' })
+const lightBarId = appendChartContainer({ idNum: 1, chartTitle: 'All Light - Bar' })
 appendChartContainer({ idNum: 10, chartTitle: 'All Light - Scatter' })
 appendChartContainer({ idNum: 100, chartTitle: 'All Light - Line' })
 appendChartContainer({ idNum: 2, chartTitle: 'Strong Dark - Bar' })
@@ -18,5 +26,19 @@ appendChartContainer({ idNum: 40, chartTitle: 'Gradient Dark - Scatter' })
 appendChartContainer({ idNum: 400, chartTitle: 'Gradient Dark - Line' })
 
 getData().then(data => {
-    console.log(data);
+    let barData = data
+        .filter(d => d.year === '2021')
+        .sort((a, b) => b.average - a.average)
+    barData = [2, 17, 30, 50, 111, 147, 201].map(i => barData[i])
+    const barMargin = getMargin({ left: 108 })
+
+    addBar(
+        getChart({
+            id: lightBarId,
+            chartDimensions: getChartDimensions({ chartId: lightBarId }),
+            margin: barMargin
+        }),
+        barData
+    )
+
 })

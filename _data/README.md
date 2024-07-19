cbf1b83657e1536d32b6d635f98fd2c44ebf413c
boa50 <bruno.albuquerque50@gmail.com>
2024-07-19 14:21:49 -0300

    Organised the gdp per capita dataset

--- _data/README.md
+++ _data/README.md
@@ -5,3 +5,5 @@ Sources:
   - Human Mortality Database (2023); United Nations - World Population Prospects (2022) – with minor processing by Our World in Data
  - gov-spending.csv (transformed from historical-gov-spending-gdp.csv[https://ourworldindata.org/grapher/historical-gov-spending-gdp])
   - International Monetary Fund (2023) – with minor processing by Our World in Data
+ - gdp-per-capita.csv (transformed from historical-gov-spending-gdp.csv[https://ourworldindata.org/grapher/gdp-per-capita-worldbank])
+  - World Bank (2023) – with minor processing by Our World in Data
--- _data/etl.py
+++ _data/etl.py
@@ -6,14 +6,61 @@ def get_path(file_name):
     return path.join(path.dirname(path.realpath(__file__)), file_name)
 
 
+def print_df_info(df):
+    print()
+    print("### NAN TOTAL ###")
+    print(df.isna().sum())
+    print("### HEAD ###")
+    print(df.head())
+    print("### SHAPE ###")
+    print(df.shape)
+
+
 ### gov-spending
-df = pd.read_csv(get_path("historical-gov-spending-gdp.csv"))
+try:
+    df = pd.read_csv(get_path("historical-gov-spending-gdp.csv"))
 
-df = df.drop("Code", axis=1)
+    df = df.drop("Code", axis=1)
 
-df.columns = [["entity", "year", "govSpending"]]
+    df.columns = ["entity", "year", "govSpending"]
 
-print(df.head())
-print(df.isna().sum())
+    print_df_info(df)
 
-df.to_csv(get_path("gov-spending.csv"), index=False)
+    df.to_csv(get_path("gov-spending.csv"), index=False)
+except:
+    print("gov-spending source file not found")
+
+### gdp-per-capita
+try:
+    df = pd.read_csv(get_path("gdp-per-capita-worldbank.csv"))
+
+    df.columns = ["country", "code", "year", "gdpPerCapita"]
+
+    aggregated_entities = [
+        "World",
+        "Europe and Central Asia (WB)",
+        "European Union (27)",
+        "High-income countries",
+        "Latin America and Caribbean (WB)",
+        "Low-income countries",
+        "Lower-middle-income countries",
+        "Middle East and North Africa (WB)",
+        "Middle-income countries",
+        "North America (WB)",
+        "South Asia (WB)",
+        "Sub-Saharan Africa (WB)",
+        "Upper-middle-income countries",
+    ]
+
+    df = df[
+        (df["year"] >= 2010)
+        & (df["year"] <= 2021)
+        & (~df["country"].isin(aggregated_entities))
+        & (~df["code"].isna())
+    ].reset_index()
+
+    print_df_info(df)
+
+    df.to_csv(get_path("gdp-per-capita.csv"), index=False)
+except:
+    print("gdp-per-capita source file not found")

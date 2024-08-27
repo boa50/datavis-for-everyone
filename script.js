cf9d5a5fc20713705129af78e8962e21ccd56051
boa50 <bruno.albuquerque50@gmail.com>
2024-08-27 14:12:45 -0300

    Added the geographic data dashboard to the links menu

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const links = [
     { txt: 'The Art of Simplicity', url: './simplicity/index.html', dateUpdated: new Date(2024, 6, 10) },
     { txt: 'Light & Dark Backgrounds', url: './light-dark-background/index.html', dateUpdated: new Date(2024, 6, 12) },
     { txt: 'Categorical Distribution', url: './categorical-distribution/index.html', dateUpdated: new Date(2024, 6, 18) },
+    { txt: 'Geographic Data', url: './geographic-data/index.html', dateUpdated: new Date(2024, 7, 26) },
 ]
 
 links

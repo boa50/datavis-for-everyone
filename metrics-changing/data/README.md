b9c3ebe6f631a41bd7cfc80a861ef3c6a4401307
boa50 <bruno.albuquerque50@gmail.com>
2024-09-11 14:38:01 -0300

    Added the number of homicides to the dataset

--- metrics-changing/data/README.md
+++ metrics-changing/data/README.md
@@ -1,3 +1,5 @@
 Sources:
  - homicide-rate-unodc.csv[https://ourworldindata.org/grapher/homicide-rate-unodc] 
   - United Nations Office on Drugs and Crime (2023) – with minor processing by Our World in Data
+ - population-and-demography.csv[https://ourworldindata.org/explorers/population-and-demography]
+  - UN, World Population Prospects (2024) – processed by Our World in Data
--- metrics-changing/data/etl.py
+++ metrics-changing/data/etl.py
@@ -1,5 +1,6 @@
 from os import path
 import pandas as pd
+import numpy as np
 
 
 def get_path(file_name):
@@ -10,7 +11,7 @@ df = pd.read_csv(get_path("homicide-rate-unodc.csv"))
 
 df.columns = ["country", "code", "year", "homicideRate"]
 
-df = df.drop("code", axis=1)
+# df = df.drop("code", axis=1)
 
 countries_to_exclude = [
     "World",
@@ -19,6 +20,9 @@ countries_to_exclude = [
     "Asia (UN)",
     "Europe (UN)",
     "Oceania (UN)",
+    "Melanesia (UN)",
+    "Micronesia (UN)",
+    "Polynesia (UN)",
 ]
 
 df = df[~df["country"].isin(countries_to_exclude)]
@@ -27,6 +31,20 @@ df_max_year = df.groupby("country").max()["year"].reset_index()
 
 df = pd.merge(df, df_max_year, how="inner", on=["country", "year"])
 
+df_population = pd.read_csv(get_path("population-and-demography.csv"))
+
+df_population.columns = ["country", "code", "year", "population"]
+
+df = pd.merge(df, df_population, how="left", on=["code", "year"])
+
+df["homicideNumber"] = np.ceil(df["population"] / 1e5 * df["homicideRate"]).astype(int)
+df["homicideRate"] = np.round(df["homicideRate"], 2)
+
+df = df.drop(["code", "year", "country_y", "population"], axis=1)
+
+df = df.rename(columns={"country_x": "country"})
+
+print(df.shape)
 print(df.isna().sum())
 print(df.head())
 

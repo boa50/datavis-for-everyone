9e189fbd5b560240081d653bf9bac3f728b25d37
boa50 <bruno.albuquerque50@gmail.com>
2024-09-18 17:58:32 -0300

    Refactored the code to get responsive sizes

--- metrics-changing/sizes.js
+++ metrics-changing/sizes.js
@@ -1,10 +1,6 @@
 export const flagWidth = window.innerHeight / 18
-export const xAxisFontSize = getResponsiveSize({
-    xl2: '1.75rem', xl: '0.9rem', lg: '0.9rem', md: '0.9rem', sm: '0.9rem'
-})
-export const explanationTextFontSize = getResponsiveSize({
-    xl2: '1.75rem', xl: '1.45rem', lg: '1.45rem', md: '1.45rem', sm: '1.45rem'
-})
+export const xAxisFontSize = getResponsiveSize({ xl2: '1.75rem', sm: '0.9rem' })
+export const explanationTextFontSize = getResponsiveSize({ xl2: '1.75rem', sm: '1.45rem' })
 export const chartTitleFontSize = '1.75rem'
 export const rankingFontSize = '1.2rem'
 export const countryNameFontSize = '1rem'
@@ -14,13 +10,9 @@ export const flagToBarPadding = 4
 export const rankingToFlagPadding = 8
 export const countryNamePadding = 8
 
-export const getChartWidth = visualisationsWidth => visualisationsWidth * getResponsiveSize({
-    xl2: 0.7, xl: 0.6, lg: 0.6, md: 0.5, sm: 0.5
-})
+export const getChartWidth = visualisationsWidth => visualisationsWidth * getResponsiveSize({ xl2: 0.7, lg: 0.6, sm: 0.5 })
 export const getChartHeight = windowHeight => windowHeight - 128
-export const chartXposition = getResponsiveSize({
-    xl2: 86, xl: 86, lg: 86, md: 116, sm: 116
-})
+export const chartXposition = getResponsiveSize({ xl2: 116, lg: 86, sm: 116 })
 export const getChartYposition = windowHeight => (windowHeight - getChartHeight(windowHeight)) / 2 - 16
 
 export const getExplanationTextWidth = visualisationsWidth => visualisationsWidth - getChartWidth(visualisationsWidth) - chartXposition - 100
@@ -36,15 +28,35 @@ export const getDataSourceYposition = windowHeight => windowHeight - 24
 
 
 function getResponsiveSize(sizes = { xl2: 0, xl: 0, lg: 0, md: 0, sm: 0 }) {
+    const sizeKeys = Object.keys(sizes)
+
+    const getAvailableSize = screenSize => {
+        switch (screenSize) {
+            case 'xl2':
+                if (sizeKeys.includes('xl2')) return sizes.xl2
+            case 'xl':
+                if (sizeKeys.includes('xl')) return sizes.xl
+            case 'lg':
+                if (sizeKeys.includes('lg')) return sizes.lg
+            case 'md':
+                if (sizeKeys.includes('md')) return sizes.md
+            case 'sm':
+                if (sizeKeys.includes('sm')) return sizes.sm
+            default:
+                break
+        }
+    }
+
+
     if (window.matchMedia("(min-width: 1536px)").matches) {
-        return sizes.xl2
+        return getAvailableSize('xl2')
     } else if (window.matchMedia("(min-width: 1280px)").matches) {
-        return sizes.xl
+        return getAvailableSize('xl')
     } else if (window.matchMedia("(min-width: 1024px)").matches) {
-        return sizes.lg
+        return getAvailableSize('lg')
     } else if (window.matchMedia("(min-width: 768px)").matches) {
-        return sizes.md
+        return getAvailableSize('md')
     } else {
-        return sizes.sm
+        return getAvailableSize('sm')
     }
 }
